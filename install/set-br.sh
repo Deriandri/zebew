d41b6b2f09a4d6073a765423c0a2351b60c24e57
Deriandri <deriandriananda82@gmail.com>
2024-12-10 20:02:16 +0700

    Update set-br.sh

--- install/set-br.sh
+++ install/set-br.sh
@@ -2,16 +2,16 @@
 
 apt install rclone
 printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/sehuadri/new/main/install/rclone.conf"
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/Deriandri/zebew/main/install/rclone.conf"
 git clone  https://github.com/casper9/wondershaper.git
 cd wondershaper
 make install
 cd
 rm -rf wondershaper
 cd /usr/bin
-wget -O backup "https://raw.githubusercontent.com/sehuadri/new/main/menu/backup.sh"
-wget -O restore "https://raw.githubusercontent.com/sehuadri/new/main/menu/restore.sh"
-wget -O xp "https://raw.githubusercontent.com/sehuadri/new/main/install/xp.sh"
+wget -O backup "https://raw.githubusercontent.com/Deriandri/zebew/main/menu/backup.sh"
+wget -O restore "https://raw.githubusercontent.com/Deriandri/zebew/main/menu/restore.sh"
+wget -O xp "https://raw.githubusercontent.com/Deriandri/zebew/main/install/xp.sh"
 chmod +x /usr/bin/backup
 chmod +x /usr/bin/restore
 chmod +x /usr/bin/xp
@@ -49,7 +49,7 @@ service cron restart > /dev/null 2>&1
 
 # > Pasang Limit
 
-#wget "https://raw.githubusercontent.com/sehuadri/new/main/bin/limit.sh" >/dev/null 2>&1
+#wget "https://raw.githubusercontent.com/Deriandri/zebew/main/bin/limit.sh" >/dev/null 2>&1
 
 #chmod +x limit.sh && bash limit.sh >/dev/null 2>&1
     
